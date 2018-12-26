703c7142945f5ee818bede174b3a2d734f7d7856
denak <dena.kuts1@gmail.com>
2018-12-27 01:00:23 +0200

    minor fixes

--- app/src/main/java/com/chnu/ejournal/entities/Subject.kt
+++ app/src/main/java/com/chnu/ejournal/entities/Subject.kt
@@ -13,7 +13,7 @@ import kotlin.collections.ArrayList
  * time - year, month, day, hour and minute of the start of the subject
  * image - id of the background image of the subject
  */
-data class Subject(val name: String, val group: Group, val time: Date, val image: Int, val lessonNumber: Int) {
+data class Subject(val name: String, val group: Group, val time: Date, val image: Int, val lessonNumber: Int): Comparable<Subject> {
     val labs = ArrayList<Lab>()
 
     fun getImage(context: Context): Drawable {
@@ -23,17 +23,39 @@ data class Subject(val name: String, val group: Group, val time: Date, val image
         return drawable!!
     }
 
-    fun getPrimaryImageColor(context: Context): Int{
+    fun getPrimaryImageColor(context: Context): Int {
         val array = context.resources.obtainTypedArray(R.array.primary_image_colors)
         val drawable = array.getColor(image, -1)
         array.recycle()
         return drawable
     }
 
-    fun getSecondaryImageColor(context: Context): Int{
+    fun getSecondaryImageColor(context: Context): Int {
         val array = context.resources.obtainTypedArray(R.array.secondary_image_colors)
         val drawable = array.getColor(image, -1)
         array.recycle()
         return drawable
+    }
+
+    override operator fun compareTo(other: Subject): Int {
+        val calendar = Calendar.getInstance()
+        calendar.time = time
+
+        val otherCalendar = Calendar.getInstance()
+        otherCalendar.time = other.time
+        val day = calendar.get(Calendar.DAY_OF_YEAR)
+        val otherDay = otherCalendar.get(Calendar.DAY_OF_YEAR)
+        return when {
+            day > otherDay -> 1
+            day < otherDay -> -1
+            else -> {
+                when {
+                    lessonNumber > other.lessonNumber -> 1
+                    lessonNumber < other.lessonNumber -> -1
+                    else -> 0
+                }
+            }
+
+        }
     }
 }
